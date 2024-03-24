77ed2b8e992eb148361274c10ace8857cbaf441e
jiamengpei.bj@acewill.cn <jiamengpei.bj@acewill.cn>
2024-03-24 21:40:47 +0800

    三河证书查询

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="header">
+    <div class="main"></div>
+    header
+  </div>
+</template>
+
+<style lang="less"  scoped>
+.header{
+  height: 80px;
+  .main{
+    width: 1290px;
+    margin: 0 auto;
+  }
+}
+</style>
